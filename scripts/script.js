00844e1c1b839052886889c9bd09515f0046b1e1
Ashley Clarke <crashley92@gmail.com>
2019-08-31 15:22:28 -0700

    issues with gifs not displaying and jquery saying attr() is not a function

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ console.log(selected);
 
 //stores query. A request is limited to 10 gifs and has a rating of PG or lower. 
 var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ELC8OedTaDdrG3ruUPvjtuXXUxkfrbY6&q=" + selected + "&limit=10&offset=0&rating=PG&lang=en";
-
+console.log(queryURL);
 
 //Makes an AJAX GET request to queryURL
 $.ajax({
@@ -19,16 +19,72 @@ $.ajax({
 })
 
 //after the data is called from AJAX request
-.then(function(response){
-$("#giphy-output").text(JSON.stringify(response));
-console.log(response.data);
-    });
+.then(function(response) {
+
+/*------------------DISPLAYS GIF---------------------------------------------------*/
+//stores response data 
+var gifData = response.data; 
+//tests for response data call
+console.log(gifData);
+
+//Removes previous gifs so they dont stay on the page. 
+$("#giphy-output").empty();
+
+//loop for gif display and attribute assinments
+for (var i = 0; i < gifData.length; i++) {
+
+//creates div for each displayed gif
+var displayedGifDiv = $("<div>");
+//Outputs rating listed in API
+var ratingDisplay = $("<p>").text("Rating: " + gifData[i].rating);
+//creates html where gif will be displayed
+var gifDisplay = $("<img>");
+
+//----ANIMATION STATES ASSIGNMENT------//
+
+//Image is still until animation is initated
+gifDisplay.attr("src", gifData[i].images.fixed_height_still.url);
+//where paused gif is called from API
+gifDisplay.attr("data-still", gifData[i].images.fixed_height_still.url);
+//where moving gif is called from API
+gifDisplay.attr("data-animate", gifData[i].images.fixed_height.url);
+console.log(gifData[i].images.fixed_height.url)
+//data state at still
+gifData.attr("data-state", "still");
+//makes gif class
+gifData.addClass("class", "gif");
+
+//pushes gifs to created div
+displayedGifDiv.prepend(gifDisplay);
+//pushes rating to created div
+displayedGifDiv.append(ratingDisplay);
+
 }
+
+});
+
+}//end of info display function
+
+
+/*-------------------CHANGES STATE OF ANIMATION----------------------------------- */
+//when gif class is clicked the state of still or animated gif changes
+function playGif() {
+var state = $(this).attr("data-state");
+
+if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+   } else {
+     $(this).attr("src", $(this).attr("data-still"));
+     $(this).attr("data-state", "still");
+   }
+  }
+ //END OF INFO DISPLAY FUNCTION
 /*-----------------END OF AJAX REQUEST-----------------------------------------------*/
 
 /*---------------------CREATES BUTTONS-----------------------------------------------*/
 //prevents buttons from duplicating
-$("#buttons-output").empty();
+//$("#giphy-output").empty();
 //loops throuh array and creates a button for items in array. 
 for (var i = 0; i < topics.length; i++) {
     //create button
@@ -45,8 +101,7 @@ for (var i = 0; i < topics.length; i++) {
  
 /*-------------------END OF BUTTON CREATION----------------------------------------*/
 
-/*-------------------PUTS AJAX CALL INTO DIV---------------------------------------*/
+/*-------------------PUTS AJAX CALL INTO BUTTON---------------------------------------*/
 $(document).on("click", ".topics-button", infoDisplay);
 
-
-
+$(document).on("click", ".gif", playGif);
