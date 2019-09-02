c5523eefb0aa5f5f71fcb9844f8acea4e319a931
Ashley Clarke <crashley92@gmail.com>
2019-09-02 13:49:49 -0700

    10 gifs generate, user can input a topic and it will be added to the topics array, added styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 //Array that stores topics that will be pulled from Giphy
-var topics = ["batman", "jessica jones", "deadpool", "wonder woman", "captain marvel"];
+var topics = ["batman", "jessica jones", "deadpool", "wonder woman", "captain marvel", "spiderman", "justice league", "watchmen", "green arrow", "the umbrella academy"];
 
 /*-------------------AJAX REQUEST--------------------------------------------------*/
 function infoDisplay() {
@@ -28,9 +28,8 @@ $.ajax({
 var gifData = response.data; 
 //tests for response data call
 console.log(gifData);
-
- //Removes previous gifs so they dont stay on the page. 
- $("#giphy-output").empty();
+//removes previous gifs that were displayed
+$("#giphy-output").empty();
 
 //loop for gif display and attribute assinments
 for (var i = 0; i < gifData.length; i++) {
@@ -39,6 +38,8 @@ for (var i = 0; i < gifData.length; i++) {
 var displayedGifDiv = $("<div>");
 //Outputs rating listed in API
 var ratingDisplay = $("<p>").text("Rating: " + gifData[i].rating);
+//creates class used for stlying
+ratingDisplay.addClass("gif-rating"); 
 //creates html where gif will be displayed
 var gifDisplay = $("<img>");
 
@@ -69,9 +70,13 @@ gifDisplay.addClass("gif");
 
 }//end of info display function
 
-
 /*---------------------CREATES BUTTONS-----------------------------------------------*/
 //loops throuh array and creates a button for items in array. 
+function generateButtons() {
+
+//Prevents duplication of buttons when a new topic is added. 
+$("#button-output").empty();
+
 for (var i = 0; i < topics.length; i++) {
     //create button
     var createTopicButton = $("<button>");
@@ -83,7 +88,28 @@ for (var i = 0; i < topics.length; i++) {
     createTopicButton.text(topics[i]);
     //adding button to the HTML
     $("#button-output").append(createTopicButton);
+    //adds bootstrap class to buttons
+    createTopicButton.addClass("btn btn-info")
     }
+}
+
+/*--------------------FORM INPUT-----------------------------------------------------*/
+
+$("#add-gif").on("click", function(event) {
+
+    //keeps buttons from reseting to default after form has been submitted
+    event.preventDefault();
+    
+    //takes vaule submited by user and pushed to topics array
+        var gifInput = $("#gif-input").val().trim();
+        topics.push(gifInput);
+        
+        //creates all buttons including the new topic.
+       generateButtons();
+    });
+    //calls buttons before new toic is added. 
+    generateButtons();
+
 /*-------------------END OF BUTTON CREATION----------------------------------------*/
 
 /*-------------------CHANGES STATE OF ANIMATION----------------------------------- */
@@ -102,8 +128,9 @@ if (state === "still") {
  //END OF INFO DISPLAY FUNCTION
 /*-----------------END OF AJAX REQUEST-----------------------------------------------*/
 
-/*-------------------PUTS AJAX CALL INTO BUTTON---------------------------------------*/
+//event listener for when button is clicked and displays gifs
 $(document).on("click", ".topics-button", infoDisplay);
-
+//event listener for when gif is clicked and paused gif or animates it. 
 $(document).on("click", ".gif", playGif);
 });
+
